feafca736bf6318d930815d2bcd8a9ec9f052922
Archenoth <archenoth@gmail.com>
2016-12-17 11:27:15 -0700

    jisho-definition now tells you which words we are fetching

--- src/jisho_link_fetcher/core.clj
+++ src/jisho_link_fetcher/core.clj
@@ -39,6 +39,7 @@
   [word]
   (Thread/sleep 1000) ;; Be courteous, limit at 1 per second
   (let [doc (get! (str "http://jisho.org/search?utf8=%E2%9C%93&keyword=" word))]
+    (println (str "Fetching " word "..."))
     {:word (-> (select "div.exact_block .text" doc) text first)
      :furigana (jisho-furigana doc)
      :definition (-> (select ".meaning-meaning" doc) text first)}))
