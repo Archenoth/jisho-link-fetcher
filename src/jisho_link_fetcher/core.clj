07918c4478143a52e9bc4cd84bb1b7d6043c6fc4
Archenoth <archenoth@gmail.com>
2016-12-17 11:24:48 -0700

    We really do only want the first definition

--- src/jisho_link_fetcher/core.clj
+++ src/jisho_link_fetcher/core.clj
@@ -27,7 +27,7 @@
   [word]
   (Thread/sleep 1000) ;; Be courteous, limit at 1 per second
   (let [doc (get! (str "http://jisho.org/search?utf8=%E2%9C%93&keyword=" word))]
-    {:word (-> (select "div.exact_block .text" doc) text)
+    {:word (-> (select "div.exact_block .text" doc) text first)
      :furigana (jisho-furigana doc)
      :definition (-> (select ".meaning-meaning" doc) text first)}))
 
